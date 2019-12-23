42136659de90d40ef7339437a1545885a8f70000
Giri-Pai <giri@gaindecktechnologies.com>
2019-12-24 01:57:05 +0530

    QR code generation for patient

---
+++ models/Patient.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+  registerNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  qrcode: {
+    type: String
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+
+  guardians: [
+    {
+      name: {
+        type: String
+      },
+      address: {
+        type: String
+      },
+      relationship: {
+        type: String
+      },
+      phone: {
+        type: String
+      },
+      email: {
+        type: String
+      }
+    }
+  ],
+  myHospitals: [
+    {
+      hospital: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "hospital"
+      },
+      date: {
+        type: Date
+      }
+    }
+  ],
+
+  myDoctors: [
+    {
+      doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "doctor"
+      },
+      date: {
+        type: Date
+      }
+    }
+  ],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "admin"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  access: {
+    hospitals: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "hospitals"
+      }
+    ],
+    doctors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "doctor"
+      }
+    ]
+  }
+});
+
+module.exports = mongoose.model("patient", patientSchema);
--- routes/api/patients.js
+++ routes/api/patients.js
@@ -1,9 +1,212 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   Get api/patients
-// @desc    Test Route
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const qrcode = require("qrcode");
+const { check, validationResult } = require("express-validator");
+
+// Import Middlewares
+const auth = require("../../middleware/auth");
+
+// Import Models
+const Admin = require("../../models/Admin");
+const Hospital = require("../../models/Hospital");
+const Patient = require("../../models/Patient");
+
+//Config File Upload
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads/patient/avatar");
+  },
+  filename: function(req, file, cb) {
+    if (file.mimetype === "image/jpeg") {
+      cb(null, req.body.registerNumber + ".jpg");
+    } else if (file.mimetype === "image/png") {
+      cb(null, req.body.registerNumber + ".png");
+    }
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+// End File Upload
+
+// @route   Get api/patient/
+// @desc    Get all patient
 // @access  Public
-router.get("/", (req, res) => res.send("Patients route"));
+router.get("/", async (req, res) => {
+  try {
+    const patients = await Patient.find().select(["-password"]);
+    return res.json(patients);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
+
+// @route   POST api/patients/
+// @desc    Creating a Patient
+// @access  Private (admin)
+router.post(
+  "/",
+  [
+    auth,
+    upload.single("avatar"),
+    [
+      check("name", "Name is required")
+        .not()
+        .isEmpty(),
+      check("registerNumber", "Register Number is required")
+        .not()
+        .isEmpty(),
+      check("email", "Please include a valid email").isEmail(),
+      check(
+        "password",
+        "Please enter a password with 6 or more characters"
+      ).isLength({ min: 6 }),
+      check("dob", "Please include the date")
+        .not()
+        .isEmpty(),
+      check("address", "Please enter the address")
+        .not()
+        .isEmpty(),
+      check("phone", "Please enter the phone number")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    if (!req.admin) {
+      return res
+        .status(401)
+        .json({ msg: "You are not authorized to access this url" });
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      name,
+      email,
+      password,
+      registerNumber,
+      dob,
+      gender,
+      phone,
+      address
+    } = req.body;
+
+    try {
+      let patient = await Patient.findOne({ email: email });
+
+      if (patient) {
+        return res
+          .status(400)
+          .send({ errors: [{ msg: "Hospital Already Exists" }] });
+      }
+
+      const createdBy = req.admin.id;
+      const isActive = false;
+
+      //   create instance of hospital
+      patient = new Patient({
+        name,
+        email,
+        password,
+        registerNumber,
+        dob,
+        gender,
+        phone,
+        address,
+        createdBy,
+        isActive
+      });
+
+      //bcrypt
+      const salt = await bcrypt.genSalt(10);
+      patient.password = await bcrypt.hash(password, salt);
+
+      //Save image url
+      patient.avatar = req.file.path;
+
+      patient.isActive = true;
+
+      // save patient
+      await patient.save();
+      const path = "uploads/patient/qrcodes/" + registerNumber + ".png";
+      const opts = {
+        mode: "alphanumeric",
+        errorCorrectionLevel: "H",
+        type: "image/png",
+        quality: 0.9,
+        margin: 1
+      };
+      const str = patient._id;
+      console.log(str);
+
+      await qrcode.toFile(path, `${str}`, opts);
+      createdPatient = await Patient.findOne(patient._id);
+      console.log(createdPatient);
+      createdPatient.qrcode = path;
+      createdPatient.save();
+
+      //   process.exit(1);
+      return res.json(createdPatient);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// @route   Delete api/hospitals/:hospital_id
+// @desc    Delete Hospital with ID by Admin
+// @access  Private (Admin)
+router.delete("/:hospital_id", auth, async (req, res) => {
+  try {
+    if (!req.admin) {
+      return res
+        .status(401)
+        .json({ msg: "You are not authorized to access this url" });
+    }
+
+    const hospital = await Hospital.findOneAndDelete({
+      _id: req.params.hospital_id
+    });
+
+    const createdAdmin = await Admin.findById(hospital.admin);
+
+    removeIndex = createdAdmin.hospitals
+      .map(hospital => hospital)
+      .indexOf(req.params.hospital_id);
+
+    createdAdmin.hospitals.splice(removeIndex);
+    await createdAdmin.save();
+
+    return res.status(400).json({ msg: "Deletion Successful" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
